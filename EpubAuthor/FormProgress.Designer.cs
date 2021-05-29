1768408ba2e41e7ff4ccac02021dffaa701e07c8
Dirk Prins <info@fornext.nl>
2021-05-29 21:22:43 +0200

    Add files via upload

---
+++ EpubAuthor/FormProgress.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace EpubAuthor
+{
+    partial class FormProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProgress));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProgressFromTo = new System.Windows.Forms.Label();
+            this.lblFromTo = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(455, 50);
+            this.progressBar.TabIndex = 0;
+            // 
+            // lblProgressFromTo
+            // 
+            this.lblProgressFromTo.AutoSize = true;
+            this.lblProgressFromTo.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgressFromTo.Location = new System.Drawing.Point(12, 65);
+            this.lblProgressFromTo.Name = "lblProgressFromTo";
+            this.lblProgressFromTo.Size = new System.Drawing.Size(103, 23);
+            this.lblProgressFromTo.TabIndex = 0;
+            this.lblProgressFromTo.Text = "Working:";
+            // 
+            // lblFromTo
+            // 
+            this.lblFromTo.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFromTo.Location = new System.Drawing.Point(165, 65);
+            this.lblFromTo.Name = "lblFromTo";
+            this.lblFromTo.Size = new System.Drawing.Size(303, 23);
+            this.lblFromTo.TabIndex = 0;
+            this.lblFromTo.Text = "0 From 0";
+            this.lblFromTo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCancel.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(16, 111);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(452, 34);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FormProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 157);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblFromTo);
+            this.Controls.Add(this.lblProgressFromTo);
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "FormProgress";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Progress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblProgressFromTo;
+        private System.Windows.Forms.Label lblFromTo;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ EpubAuthor/FormProgress.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EpubAuthor
+{
+    public partial class FormProgress : Form
+    {
+        #region Members
+
+        public bool cancelled;
+
+        #endregion
+
+        #region Constructor
+
+        public FormProgress()
+        {
+            InitializeComponent();
+
+            cancelled = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SetValue(int value, int total)
+        {
+            lblFromTo.Text = value.ToString() + " From " + total.ToString();
+            if (total > 0)
+            {
+                progressBar.Value = 100 * value / total;
+            }   else
+            {
+                progressBar.Value = 0;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Cancel receiving mails
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cancelled = true;
+            Close();
+        }
+
+        #endregion
+    }
+}
